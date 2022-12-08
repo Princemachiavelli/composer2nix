a9812735a8b92a186eb3ee1fb5290c13a846eda4
Josh Hoffer <jhoffer@sansorgan.es>
2022-12-08 16:54:56 -0700

    make overlay independent of system

--- flake.nix
+++ flake.nix
@@ -5,11 +5,21 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  let
+      overlays = final: prev: {
+        composer2nix = prev.callPackage ./default.nix { };
+        composer2nix-noDev = prev.callPackage ./default.nix { noDev = true; };
+      };
+  in
+  {
+    overlays.default = overlays;
+  } // flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        composer2nix = import ./default.nix { inherit pkgs; };
-        composer2nix-noDev = import ./default.nix { inherit pkgs; noDev = true; };
+        pkgs = import nixpkgs {
+          inherit system self;
+          overlays = [ self.overlays.default ];
+        };
+      in with pkgs; rec {
         app = flake-utils.lib.mkApp {
           drv = composer2nix;
           exePath = "/bin/composer2nix";
@@ -18,17 +28,11 @@
           drv = composer2nix-noDev;
           exePath = "/bin/composer2nix";
         };
-        overlays = final: prev: {
-          composer2nix = composer2nix;
-          composer2nix-noDev = composer2nix-noDev;
-        };
-      in {
         packages.composer2nix = composer2nix;
         packages.composer2nix-noDev = composer2nix-noDev;
         defaultPackage = composer2nix;
         apps.composer2nix = app;
         apps.composer2nix-noDev = app-noDev;
         defaultApp = app;
-        overlays.default = overlays;
       });
 }
